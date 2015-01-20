ea8e60f21cb54d974db10385e5c45cd590d8fadb
Sergey A Volkov <serg-v@yandex-team.ru>
2015-01-20 17:37:21 +0300

    added license

--- src/ru/volkov/idea/jsoncreator/JsonCreatorHandler.java
+++ src/ru/volkov/idea/jsoncreator/JsonCreatorHandler.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2000-2009 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ru.volkov.idea.jsoncreator;
 
 import org.jetbrains.annotations.NonNls;
@@ -20,6 +36,7 @@ import com.intellij.psi.util.TypeConversionUtil;
 import com.intellij.util.IncorrectOperationException;
 
 /**
+ * Some static code copied from GenerateConstructorHandler.
  * User: serg-v
  * Date: 12/15/14
  * Time: 3:12 AM
